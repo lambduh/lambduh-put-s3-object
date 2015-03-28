91349d3a1ba466819f395ff87bcadd2ef6d00670
Russell Matney <russell.matney@gmail.com>
2015-03-28 19:18:42 -0400

    implements result-flow-refactor

--- index.js
+++ index.js
@@ -3,43 +3,47 @@ var mime = require('mime');
 var AWS = require('aws-sdk');
 var fs = require('fs');
 
-module.exports = function() {
-  return function(options) {
-    var def = Q.defer();
+module.exports = function(result, options) {
+  if (!result) { result = {} };
+  var def = Q.defer();
 
-    if (!options) {
-      def.reject(new Error("S3 Upload expected options object."));
-    } else if (!options.dstBucket) {
-      def.reject(new Error("S3 Upload expected options.dstBucket to exist"));
-    } else if (!options.dstKey) {
-      def.reject(new Error("S3 Upload expected options.dstKey to exist"));
-    } else if (!options.uploadFilepath) {
-      def.reject(new Error("S3 Upload expected options.uploadFilepath to exist"));
-    } else {
+  if (!options) {
+    def.reject(new Error("S3 Upload expected options object."));
+  } else if (!options.dstBucket) {
+    def.reject(new Error("S3 Upload expected options.dstBucket to exist"));
+  } else if (!options.dstKey) {
+    def.reject(new Error("S3 Upload expected options.dstKey to exist"));
+  } else if (!options.uploadFilepath) {
+    def.reject(new Error("S3 Upload expected options.uploadFilepath to exist"));
+  } else {
 
-      var params = {
-        Bucket: options.dstBucket,
-        Key: options.dstKey,
-        ContentType: mime.lookup(options.uploadFilepath)
-      }
-
-      var file = fs.createReadStream(options.uploadFilepath);
-      var S3 = new AWS.S3({params: params});
-      S3.upload({Body: file})
-        .on('httpUploadProgress', function(evt) {
-          console.log('Upload Progress: ' + (100 * evt.loaded / evt.total));
-        })
-        .send(function(err, data) {
-          if (err) {
-            def.reject(err);
-          } else {
-            console.log('successful upload');
-            def.resolve(options);
-          }
-        });
-
+    //unit test
+    for(var key in options) {
+      result[key] = options[key];
     }
 
-    return def.promise;
+    var params = {
+      Bucket: options.dstBucket,
+      Key: options.dstKey,
+      ContentType: mime.lookup(options.uploadFilepath)
+    }
+
+    var file = fs.createReadStream(options.uploadFilepath);
+    var S3 = new AWS.S3({params: params});
+    S3.upload({Body: file})
+      .on('httpUploadProgress', function(evt) {
+        console.log('Upload Progress: ' + (100 * evt.loaded / evt.total));
+      })
+      .send(function(err, data) {
+        if (err) {
+          def.reject(err);
+        } else {
+          console.log('successful upload');
+          def.resolve(options);
+        }
+      });
+
   }
+
+  return def.promise;
 }
