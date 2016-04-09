19c1ada543890affc5c3d4347ca9bab6cec9f5c5
Russell Matney <russell.matney@gmail.com>
2016-04-09 13:58:02 -0400

    refactor(ACL): leave default as-is, modify ACL if provided as an option

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var AWS = require('aws-sdk');
 var fs = require('fs');
 
 module.exports = function(result, options) {
-  if (!result) { result = {} };
+  if (!result) { result = {}; }
   var def = Q.defer();
 
   if (!options) {
@@ -17,33 +17,34 @@ module.exports = function(result, options) {
     def.reject(new Error("S3 Upload expected options.uploadFilepath to exist"));
   } else {
 
-    //unit test
-    for(var key in options) {
+    for (var key in options) {
       result[key] = options[key];
-    }
-
-    if(!options.ACL){
-      options.ACL = 'private';
     }
 
     var params = {
       Bucket: options.dstBucket,
       Key: options.dstKey,
-      ContentType: mime.lookup(options.uploadFilepath),
-      ACL: options.ACL
+      ContentType: mime.lookup(options.uploadFilepath)
+    };
+
+    if (options.ACL) {
+      params.ACL = options.ACL;
     }
 
     var file = fs.createReadStream(options.uploadFilepath);
+
+    var S3;
     if (options.region) {
-      var S3 = new AWS.S3({params: params, region: options.region});
+      S3 = new AWS.S3({params: params, region: options.region});
     } else {
-      var S3 = new AWS.S3({params: params});
+      S3 = new AWS.S3({params: params});
     }
+
     S3.upload({Body: file})
       .on('httpUploadProgress', function(evt) {
         console.log('Upload Progress: ' + (100 * evt.loaded / evt.total));
       })
-      .send(function(err, data) {
+      .send(function(err) {
         if (err) {
           def.reject(err);
         } else {
@@ -55,4 +56,4 @@ module.exports = function(result, options) {
   }
 
   return def.promise;
-}
+};
